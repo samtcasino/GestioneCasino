bcf9cc3dae877013dac975f4d67124b574986704
Carlo <carlo.pezzotti@samtrevano.ch>
2019-02-15 14:40:03 +0100

    Upload GantPlanning

--- code/testCode/email.php
+++ code/testCode/email.php
@@ -1,36 +1,37 @@
 <?php
-declare(strict_types=1);
-
-final class Email
-{
-    private $email;
-
-    private function __construct(string $email)
+     class Email
     {
-        $this->ensureIsValidEmail($email);
+        private $email;
 
-        $this->email = $email;
-    }
+        public function __construct(string $email)
+        {
+            $this->tryEmail($email);
 
-    public static function fromString(string $email): self
-    {
-        return new self($email);
-    }
+            $this->email = $email;
+        }
 
-    public function __toString(): string
-    {
-        return $this->email;
-    }
+        public function fromString(string $email)
+        {
+            return new self($email);
+        }
 
-    private function ensureIsValidEmail(string $email): void
-    {
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new InvalidArgumentException(
-                sprintf(
-                    '"%s" is not a valid email address',
-                    $email
-                )
-            );
+        public function __toString(): string
+        {
+            return $this->email;
+        }
+
+        public function tryEmail(string $email)
+        {
+            if(!(filter_var($email, FILTER_VALIDATE_EMAIL)))
+            {
+                throw new InvalidArgumentException( sprintf( '"%s" is not a valid email address',$email));
+            }  
         }
     }
-}
+
+    try{
+        $email = new Email("edede");
+    }catch (InvalidArgumentException $e) {
+        echo "Email sbagliata";
+    }
+?>
--- code/testCode/emailCheck.php
+++ code/testCode/emailCheck.php
@@ -6,19 +6,16 @@ include 'email.php';
 
 final class EmailTest extends TestCase
 {
+
     public function testCanBeCreatedFromValidEmailAddress(): void
     {
-        $this->assertInstanceOf(
-            Email::class,
-            Email::fromString('userexample.com')
-        );
+        $this->assertInstanceOf(Email::class,new Email("usernamegmail.com"));
     }
 
     public function testCannotBeCreatedFromInvalidEmailAddress(): void
     {
         $this->expectException(InvalidArgumentException::class);
-
-        Email::fromString('invalid');
+        Email::fromString('username@gmail.com');
     }
 
     public function testCanBeUsedAsString(): void
