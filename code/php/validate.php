ab8a5a805beaf28c8d93d53d0e6a8a9323fa30dd
Carlo <carlo.pezzotti@samtrevano.ch>
2019-03-04 15:17:25 +0100

    fix errors

---
+++ code/php/validate.php
@@ -0,0 +1,16 @@
+<?php
+	require "database/database.php";
+	if (isset($_GET['id'])) {
+	    $id = $_GET['id'];
+	    $email = urldecode($id);
+	    $email = $email ^ "SGG<?rpF3FTebqx?(kgQR:hsq'mqZ!VH";
+
+	    $database = new Database("127.0.0.1",3306,"cashyland","root","root");
+	    if(!(gettype($database->existsUserByEmail($email)) == "boolean")){
+	    	$database->executeQuery("update users SET verified = 1 where email = '". $email."'");
+	    	echo $email . " ti sei registrato con successo";
+	    }else{
+	    	echo "Qualcosa è andato storto :(";
+	    }
+	}
+?>
