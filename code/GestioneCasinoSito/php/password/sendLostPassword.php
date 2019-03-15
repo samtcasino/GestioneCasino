67492f13d3cc47198a283cb073476c5db2ccce02
MattiaToscanelli <37306216+MattiaToscanelli@users.noreply.github.com>
2019-03-15 16:00:04 +0100

    update website

---
+++ code/GestioneCasinoSito/php/password/sendLostPassword.php
@@ -0,0 +1,16 @@
+<?php
+	require "../loader.php";
+	if($_SERVER['REQUEST_METHOD'] == "POST"){
+		if(isset($_POST["email"])){
+			User::tryEmail($_POST["email"]);
+			$cryptedMail = $_POST["email"] ^ "SGG<?rpF3FTebqx?(kgQR:hsq'mqZ!VH";
+			$message = 'Change password:<a href="http://cashyland.tk/changePassword.php?id='.urlencode($cryptedMail).'">Click me!</a>';
+			$mailSender->mailSend($_POST["email"],"Reset password!",$message);
+			header("Location: ../../verifyMail.html");
+		}else{
+			echo "Ops";
+		}
+	}else{
+		echo "Ops";
+	}
+?>
