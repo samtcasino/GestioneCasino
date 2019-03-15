1f52b710d984b278bff9ff95c49759972b9f7a9b
Carlo <carlo.pezzotti@samtrevano.ch>
2019-03-15 14:20:40 +0100

    Fix errors

--- code/html/GestioneCasinoFull/php/login/register.php
+++ code/html/GestioneCasinoFull/php/login/register.php
@@ -33,7 +33,7 @@
 				);		
 				$db->insertUser($u); 
 				$cryptedMail = $_POST["email"] ^ $privateKey;
-				$message = '<hr>How are you? <br> <hr>This i your link:<a href="http://localhost:8081/php/login/validate.php?id='.urlencode($cryptedMail).'">Click me!</a>';
+				$message = '<hr>How are you? <br> <hr>This i your link:<a href="http://cashyland.tk/php/login/validate.php?id='.urlencode($cryptedMail).'">Click me!</a>';
 				$subjet = "Hi there! Verify your email :)";
 				$mailSender->mailSend($_POST["email"],$subjet,$message);
 				header("Location: ../../../verifyMail.html");	
