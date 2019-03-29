eeca2753eb4b5a337ef82ee4ced2333a48807c9d
Carlo <carlo.pezzotti@samtrevano.ch>
2019-03-29 15:36:19 +0100

    Upload website

--- code/gestioneCasinoSite/php/password/sendLostPassword.php
+++ code/gestioneCasinoSite/php/password/sendLostPassword.php
@@ -4,8 +4,8 @@
 		if(isset($_POST["email"])){
 			User::tryEmail($_POST["email"]);
 			$cryptedMail = $_POST["email"] ^ "SGG<?rpF3FTebqx?(kgQR:hsq'mqZ!VH";
-			$message = 'Change password:<a href="http://cashyland.tk/resetPassword.php?id='.urlencode($cryptedMail).'">Click me!</a>';
-			//$mailSender->mailSend($_POST["email"],"Reset password!",$message);
+			$message = 'Change password:<a href="http://cashyland.tk/changePassword.php?id='.urlencode($cryptedMail).'">Click me!</a>';
+			$mailSender->mailSend($_POST["email"],"Reset password!",$message);
 			header("Location: ../../verifyMail.html");
 		}else{
 			header("Location: ../../index.html");
