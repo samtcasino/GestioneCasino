0ef0fa53e364a51bcd701db4362c751dbfdce64b
matteoforni <matteo.forni@samtrevano.ch>
2019-04-12 14:13:03 +0200

    Create SeleniumRegistrationTest

---
+++ code/java/selenium/chrome/navigation/SeleniumTest/src/SeleniumRegistrationTest.java
@@ -0,0 +1,6 @@
+public class SeleniumRegistrationTest {
+    public static void main(String[] args) {
+        SeleniumRegistrationTestTest st = new SeleniumRegistrationTestTest();
+        st.test();
+    }
+}
