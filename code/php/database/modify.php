2c22826f6f5e66fbebc3b0eaa3a7d1793275541a
Carlo <carlo.pezzotti@samtrevano.ch>
2019-04-12 15:56:52 +0200

    pota

--- code/php/database/modify.php
+++ code/php/database/modify.php
@@ -1,7 +1,8 @@
 <?php
+    /*
     if(isset($_GET["modify"])){
         $db->executeQuery("");
     }else if(isset($_GET["delete"])){
-        $db->executeQuery();
-    }
+        $db->executeQuery("");
+    }*/
 ?>
--- code/php/tests/databaseCheck.php
+++ code/php/tests/databaseCheck.php
@@ -31,7 +31,7 @@ final class DatabaseTestCase extends TestCase
             "male",
             "Password&1"
         ));
-	    $result = $db->executeQuery("select email from user where email = 'a@a.ch'")["email"];
+	    $result = $db->executeQueryWithoutFetch("select email from user where email = 'a@a.ch'")->fetch()["email"];
         $this->assertTrue($result == "a@a.ch");
 	    $db->executeQuery("delete from user where email = 'a@a.ch'");
     }
@@ -44,19 +44,19 @@ final class DatabaseTestCase extends TestCase
 
     public function testCorrectReturn():void{
         $db = new Database("127.0.0.1",3306,"cashyland","casinoAdmin","Casin02018"); 
-        $result = $db->executeQuery("select email,name from user where email = 'admin@admin.ch'");
+        $result = $db->executeQueryWithoutFetch("select email,name from user where email = 'admin@admin.ch'")->fetch();
         $this->assertTrue(($result["email"] == "admin@admin.ch" && $result["name"] == "admin"));
     }
 
     public function testNotCorrectReturn():void{
         $db = new Database("127.0.0.1",3306,"cashyland","casinoAdmin","Casin02018"); 
-        $result = $db->executeQuery("select email,name from user where email = 'admin@admin.ch'");
+        $result = $db->executeQueryWithoutFetch("select email,name from user where email = 'admin@admin.ch'")->fetch();
         $this->assertTrue(!($result["email"] == "adm@admin.ch" && $result["name"] == "ain"));
     }
 
     public function testExecuteNotCorrectQuery(){
         $db = new Database("127.0.0.1",3306,"cashyland","casinoAdmin","Casin02018"); 
         $this->expectException(InvalidArgumentException::class);
-        $db->executeQuery("select * from users");
+        $db->executeQueryWithoutFetch("select * from users");
     }
 }
