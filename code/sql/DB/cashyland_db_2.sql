d44cf423f386d26d9f04ac562c0ad106445d2772
Matan Davidi <davidi.matan@gmail.com>
2019-04-16 18:40:55 +0200

    Fix table creation order

--- code/sql/DB/cashyland_db_2.sql
+++ code/sql/DB/cashyland_db_2.sql
@@ -8,6 +8,18 @@ USE `cashyland`;
 -- Server version	8.0.12
 
 --
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+
+CREATE TABLE `room` (
+  `location` varchar(45) NOT NULL,
+  `description` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`location`)
+);
+
+--
 -- Table structure for table `game`
 --
 
@@ -20,6 +32,31 @@ CREATE TABLE `game` (
   PRIMARY KEY (`name`,`room`),
   KEY `fk_room_idx` (`room`),
   CONSTRAINT `fk_room` FOREIGN KEY (`room`) REFERENCES `room` (`location`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+--
+-- Table structure for table `media_type`
+--
+
+DROP TABLE IF EXISTS `media_type`;
+
+CREATE TABLE `media_type` (
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`name`)
+);
+
+--
+-- Table structure for table `media`
+--
+
+DROP TABLE IF EXISTS `media`;
+
+CREATE TABLE `media` (
+  `url` varchar(100) NOT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`url`),
+  KEY `type` (`type`),
+  CONSTRAINT `media_ibfk_1` FOREIGN KEY (`type`) REFERENCES `media_type` (`name`) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 --
@@ -45,31 +82,6 @@ DROP TABLE IF EXISTS `gender`;
 
 CREATE TABLE `gender` (
   `name` varchar(10) NOT NULL,
-  PRIMARY KEY (`name`)
-);
-
---
--- Table structure for table `media`
---
-
-DROP TABLE IF EXISTS `media`;
-
-CREATE TABLE `media` (
-  `url` varchar(100) NOT NULL,
-  `type` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`url`),
-  KEY `type` (`type`),
-  CONSTRAINT `media_ibfk_1` FOREIGN KEY (`type`) REFERENCES `media_type` (`name`) ON DELETE SET NULL UPDATE CASCADE
-);
-
---
--- Table structure for table `media_type`
---
-
-DROP TABLE IF EXISTS `media_type`;
-
-CREATE TABLE `media_type` (
-  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`name`)
 );
 
@@ -102,45 +114,14 @@ CREATE TABLE `promotion_media` (
 );
 
 --
--- Table structure for table `promotion_user`
+-- Table structure for table `user_type`
 --
 
-DROP TABLE IF EXISTS `promotion_user`;
+DROP TABLE IF EXISTS `user_type`;
 
-CREATE TABLE `promotion_user` (
-  `user_type` varchar(45) NOT NULL,
-  `promotion_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_type`,`promotion_id`),
-  KEY `promotion_id` (`promotion_id`),
-  CONSTRAINT `promotion_user_ibfk_1` FOREIGN KEY (`user_type`) REFERENCES `user` (`type`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `promotion_user_ibfk_2` FOREIGN KEY (`promotion_id`) REFERENCES `promotion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
---
--- Table structure for table `room`
---
-
-DROP TABLE IF EXISTS `room`;
-
-CREATE TABLE `room` (
-  `location` varchar(45) NOT NULL,
-  `description` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`location`)
-);
-
---
--- Table structure for table `room_media`
---
-
-DROP TABLE IF EXISTS `room_media`;
-
-CREATE TABLE `room_media` (
-  `room_location` varchar(45) NOT NULL,
-  `media_url` varchar(100) NOT NULL,
-  PRIMARY KEY (`room_location`,`media_url`),
-  KEY `media_url` (`media_url`),
-  CONSTRAINT `room_media_ibfk_1` FOREIGN KEY (`room_location`) REFERENCES `room` (`location`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `room_media_ibfk_2` FOREIGN KEY (`media_url`) REFERENCES `media` (`url`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `user_type` (
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`name`)
 );
 
 --
@@ -171,12 +152,31 @@ CREATE TABLE `user` (
 );
 
 --
--- Table structure for table `user_type`
+-- Table structure for table `promotion_user`
 --
 
-DROP TABLE IF EXISTS `user_type`;
+DROP TABLE IF EXISTS `promotion_user`;
 
-CREATE TABLE `user_type` (
-  `name` varchar(45) NOT NULL,
-  PRIMARY KEY (`name`)
+CREATE TABLE `promotion_user` (
+  `user_type` varchar(45) NOT NULL,
+  `promotion_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_type`,`promotion_id`),
+  KEY `promotion_id` (`promotion_id`),
+  CONSTRAINT `promotion_user_ibfk_1` FOREIGN KEY (`user_type`) REFERENCES `user` (`type`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `promotion_user_ibfk_2` FOREIGN KEY (`promotion_id`) REFERENCES `promotion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+--
+-- Table structure for table `room_media`
+--
+
+DROP TABLE IF EXISTS `room_media`;
+
+CREATE TABLE `room_media` (
+  `room_location` varchar(45) NOT NULL,
+  `media_url` varchar(100) NOT NULL,
+  PRIMARY KEY (`room_location`,`media_url`),
+  KEY `media_url` (`media_url`),
+  CONSTRAINT `room_media_ibfk_1` FOREIGN KEY (`room_location`) REFERENCES `room` (`location`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `room_media_ibfk_2` FOREIGN KEY (`media_url`) REFERENCES `media` (`url`) ON DELETE CASCADE ON UPDATE CASCADE
 );
