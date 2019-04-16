74ecc3c8e409d17c6cd73e044c062438ba064be3
MatanDavidi <davidi.matan@gmail.com>
2019-04-16 18:19:24 +0200

    Fix db creation script syntax and details

--- code/sql/DB/cashyland_db_2.sql
+++ code/sql/DB/cashyland_db_2.sql
@@ -1,331 +1,182 @@
+CREATE DATABASE  IF NOT EXISTS `cashyland`;
+USE `cashyland`;
+
 -- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: cashyland
 -- ------------------------------------------------------
 -- Server version	8.0.12
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8mb4 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `game`
 --
 
 DROP TABLE IF EXISTS `game`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `game` (
-  `room` varchar(256) NOT NULL,
-  `name` varchar(256) NOT NULL,
+  `room` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
   `description` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`name`,`room`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `game`
---
-
-LOCK TABLES `game` WRITE;
-/*!40000 ALTER TABLE `game` DISABLE KEYS */;
-/*!40000 ALTER TABLE `game` ENABLE KEYS */;
-UNLOCK TABLES;
+  PRIMARY KEY (`name`,`room`),
+  KEY `fk_room_idx` (`room`),
+  CONSTRAINT `fk_room` FOREIGN KEY (`room`) REFERENCES `room` (`location`) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 --
 -- Table structure for table `game_media`
 --
 
 DROP TABLE IF EXISTS `game_media`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `game_media` (
-  `game_name` varchar(256) NOT NULL,
-  `media_url` varchar(256) NOT NULL,
+  `game_name` varchar(45) NOT NULL,
+  `media_url` varchar(100) NOT NULL,
   PRIMARY KEY (`game_name`,`media_url`),
   KEY `game_media_media_idx` (`media_url`),
-  CONSTRAINT `game_media_ibfk_1` FOREIGN KEY (`media_url`) REFERENCES `media` (`url`),
-  CONSTRAINT `game_media_ibfk_2` FOREIGN KEY (`game_name`) REFERENCES `game` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `game_media`
---
-
-LOCK TABLES `game_media` WRITE;
-/*!40000 ALTER TABLE `game_media` DISABLE KEYS */;
-/*!40000 ALTER TABLE `game_media` ENABLE KEYS */;
-UNLOCK TABLES;
+  CONSTRAINT `game_media_ibfk_1` FOREIGN KEY (`media_url`) REFERENCES `media` (`url`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `game_media_ibfk_2` FOREIGN KEY (`game_name`) REFERENCES `game` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 --
 -- Table structure for table `gender`
 --
 
 DROP TABLE IF EXISTS `gender`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `gender` (
   `name` varchar(10) NOT NULL,
   PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `gender`
---
-
-LOCK TABLES `gender` WRITE;
-/*!40000 ALTER TABLE `gender` DISABLE KEYS */;
-/*!40000 ALTER TABLE `gender` ENABLE KEYS */;
-UNLOCK TABLES;
+);
 
 --
 -- Table structure for table `media`
 --
 
 DROP TABLE IF EXISTS `media`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `media` (
-  `url` varchar(256) NOT NULL,
-  `type` varchar(256) DEFAULT NULL,
+  `url` varchar(100) NOT NULL,
+  `type` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`url`),
   KEY `type` (`type`),
-  CONSTRAINT `media_ibfk_1` FOREIGN KEY (`type`) REFERENCES `media_type` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `media`
---
-
-LOCK TABLES `media` WRITE;
-/*!40000 ALTER TABLE `media` DISABLE KEYS */;
-/*!40000 ALTER TABLE `media` ENABLE KEYS */;
-UNLOCK TABLES;
+  CONSTRAINT `media_ibfk_1` FOREIGN KEY (`type`) REFERENCES `media_type` (`name`) ON DELETE SET NULL UPDATE CASCADE
+);
 
 --
 -- Table structure for table `media_type`
 --
 
 DROP TABLE IF EXISTS `media_type`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `media_type` (
-  `name` varchar(256) NOT NULL,
+  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `media_type`
---
-
-LOCK TABLES `media_type` WRITE;
-/*!40000 ALTER TABLE `media_type` DISABLE KEYS */;
-/*!40000 ALTER TABLE `media_type` ENABLE KEYS */;
-UNLOCK TABLES;
+);
 
 --
 -- Table structure for table `promotion`
 --
 
 DROP TABLE IF EXISTS `promotion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `promotion` (
   `id` int(11) NOT NULL,
-  `name` varchar(256) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
   `description` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `promotion`
---
-
-LOCK TABLES `promotion` WRITE;
-/*!40000 ALTER TABLE `promotion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `promotion` ENABLE KEYS */;
-UNLOCK TABLES;
+);
 
 --
 -- Table structure for table `promotion_media`
 --
 
 DROP TABLE IF EXISTS `promotion_media`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `promotion_media` (
   `promotion_id` int(11) NOT NULL,
-  `media_url` varchar(256) NOT NULL,
+  `media_url` varchar(100) NOT NULL,
   PRIMARY KEY (`promotion_id`,`media_url`),
   KEY `media_url` (`media_url`),
-  CONSTRAINT `promotion_media_ibfk_1` FOREIGN KEY (`promotion_id`) REFERENCES `promotion` (`id`),
-  CONSTRAINT `promotion_media_ibfk_2` FOREIGN KEY (`media_url`) REFERENCES `media` (`url`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `promotion_media`
---
-
-LOCK TABLES `promotion_media` WRITE;
-/*!40000 ALTER TABLE `promotion_media` DISABLE KEYS */;
-/*!40000 ALTER TABLE `promotion_media` ENABLE KEYS */;
-UNLOCK TABLES;
+  CONSTRAINT `promotion_media_ibfk_1` FOREIGN KEY (`promotion_id`) REFERENCES `promotion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `promotion_media_ibfk_2` FOREIGN KEY (`media_url`) REFERENCES `media` (`url`) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 --
 -- Table structure for table `promotion_user`
 --
 
 DROP TABLE IF EXISTS `promotion_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `promotion_user` (
-  `user_type` varchar(256) NOT NULL,
+  `user_type` varchar(45) NOT NULL,
   `promotion_id` int(11) NOT NULL,
   PRIMARY KEY (`user_type`,`promotion_id`),
   KEY `promotion_id` (`promotion_id`),
-  CONSTRAINT `promotion_user_ibfk_1` FOREIGN KEY (`user_type`) REFERENCES `user` (`type`),
-  CONSTRAINT `promotion_user_ibfk_2` FOREIGN KEY (`promotion_id`) REFERENCES `promotion` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `promotion_user`
---
-
-LOCK TABLES `promotion_user` WRITE;
-/*!40000 ALTER TABLE `promotion_user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `promotion_user` ENABLE KEYS */;
-UNLOCK TABLES;
+  CONSTRAINT `promotion_user_ibfk_1` FOREIGN KEY (`user_type`) REFERENCES `user` (`type`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `promotion_user_ibfk_2` FOREIGN KEY (`promotion_id`) REFERENCES `promotion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 --
 -- Table structure for table `room`
 --
 
 DROP TABLE IF EXISTS `room`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `room` (
-  `location` varchar(256) NOT NULL,
+  `location` varchar(45) NOT NULL,
   `description` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`location`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `room`
---
-
-LOCK TABLES `room` WRITE;
-/*!40000 ALTER TABLE `room` DISABLE KEYS */;
-/*!40000 ALTER TABLE `room` ENABLE KEYS */;
-UNLOCK TABLES;
+);
 
 --
 -- Table structure for table `room_media`
 --
 
 DROP TABLE IF EXISTS `room_media`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `room_media` (
-  `room_location` varchar(256) NOT NULL,
-  `media_url` varchar(256) NOT NULL,
+  `room_location` varchar(45) NOT NULL,
+  `media_url` varchar(100) NOT NULL,
   PRIMARY KEY (`room_location`,`media_url`),
   KEY `media_url` (`media_url`),
-  CONSTRAINT `room_media_ibfk_1` FOREIGN KEY (`room_location`) REFERENCES `room` (`location`),
-  CONSTRAINT `room_media_ibfk_2` FOREIGN KEY (`media_url`) REFERENCES `media` (`url`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `room_media`
---
-
-LOCK TABLES `room_media` WRITE;
-/*!40000 ALTER TABLE `room_media` DISABLE KEYS */;
-/*!40000 ALTER TABLE `room_media` ENABLE KEYS */;
-UNLOCK TABLES;
+  CONSTRAINT `room_media_ibfk_1` FOREIGN KEY (`room_location`) REFERENCES `room` (`location`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `room_media_ibfk_2` FOREIGN KEY (`media_url`) REFERENCES `media` (`url`) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 --
 -- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `user` (
-  `name` varchar(256) DEFAULT NULL,
-  `surname` varchar(256) DEFAULT NULL,
-  `street` varchar(256) DEFAULT NULL,
-  `house_number` varchar(256) DEFAULT NULL,
-  `zip_code` varchar(256) DEFAULT NULL,
-  `city` varchar(256) DEFAULT NULL,
-  `email` varchar(256) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `surname` varchar(45) DEFAULT NULL,
+  `street` varchar(45) DEFAULT NULL,
+  `house_number` varchar(4) DEFAULT NULL,
+  `zip_code` varchar(5) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
   `phone_number` varchar(15) DEFAULT NULL,
-  `gender` varchar(256) DEFAULT NULL,
-  `password` varchar(256) DEFAULT NULL,
-  `type` varchar(256) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
   `admin` tinyint(1) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
   PRIMARY KEY (`email`),
   KEY `gender` (`gender`),
   KEY `type` (`type`),
-  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`gender`) REFERENCES `gender` (`name`),
-  CONSTRAINT `user_ibfk_2` FOREIGN KEY (`type`) REFERENCES `user_type` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`gender`) REFERENCES `gender` (`name`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `user_ibfk_2` FOREIGN KEY (`type`) REFERENCES `user_type` (`name`) ON DELETE SET NULL ON UPDATE CASCADE
+);
 
 --
 -- Table structure for table `user_type`
 --
 
 DROP TABLE IF EXISTS `user_type`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `user_type` (
-  `name` varchar(256) NOT NULL,
+  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_type`
---
-
-LOCK TABLES `user_type` WRITE;
-/*!40000 ALTER TABLE `user_type` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_type` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-04-12 15:03:16
+);
