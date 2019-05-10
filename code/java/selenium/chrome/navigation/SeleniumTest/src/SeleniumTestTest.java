233db84761ecae1072b435689e9c2aecede78aea
Thor Dublin <42935509+ThorDublin@users.noreply.github.com>
2019-05-10 15:38:14 +0200

    Update SeleniumTestTest.java

--- code/java/selenium/chrome/navigation/SeleniumTest/src/SeleniumTestTest.java
+++ code/java/selenium/chrome/navigation/SeleniumTest/src/SeleniumTestTest.java
@@ -81,23 +81,22 @@ class SeleniumTestTest {
 
     @Test
     void test() throws IOException {
+        /**
         ChromeDriverService options = new ChromeDriverService.Builder()
         .usingDriverExecutable(new File("/usr/bin/chromedriver"))
         .usingAnyFreePort().withEnvironment(ImmutableMap.of("DISPLAY", ":1")).build();
         options.start();
         driver = new ChromeDriver(options);
+        */
 
         final File firefoxPath = new File(System.getProperty("lmportal.deploy.firefox.path", "/usr/bin/firefox"));
         FirefoxBinary firefoxBinary = new FirefoxBinary(firefoxPath);
         String Xport = System.getProperty("lmportal.xvfb.id", ":1");
-        firefoxBinary.setEnvironmentProperty("DISPLAY", Xport);
-        driver = new FirefoxDriver(firefoxBinary, null);
 
-        driver = new FirefoxDriver( 
-                new GeckoDriverService.Builder().usingDriverExecutable(
-                new File("path/to/geckodriver.exe")).usingFirefoxBinary(
-                new FirefoxBinary(
-                new File("path/to/firefox.exe"))).withEnvironment(ImmutableMap.of("DISPLAY", "0:0")) .build());
+        driver = new FirefoxDriver( new GeckoDriverService.Builder()
+            .usingDriverExecutable(new File("/usr/bin/geckodriver"))
+            .usingFirefoxBinary(new FirefoxBinary(firefoxPath)))
+            .withEnvironment(ImmutableMap.of("DISPLAY", Xport)).build());
 
         driver.get(URL);
         waitMillis(1000);
