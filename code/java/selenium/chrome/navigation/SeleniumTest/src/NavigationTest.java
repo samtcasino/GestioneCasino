e54b6fd5116f86a26423200845bd03e8046acf3d
matteoforni <matteoforni@hotmail.com>
2019-05-20 15:39:45 +0200

    finally fix NavigationTest

--- code/java/selenium/chrome/navigation/SeleniumTest/src/NavigationTest.java
+++ code/java/selenium/chrome/navigation/SeleniumTest/src/NavigationTest.java
@@ -3,15 +3,10 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.GeckoDriverService;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.File;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NavigationTest {
@@ -21,10 +16,9 @@ class NavigationTest {
     @Test
     void home() {
         WebElement home = null;
-        home = driver.findElement(By.id("homeLi"));
-        WebElement homeLink = home.findElement(By.id("homeBtn"));
-        home.click();
+        home = driver.findElement(By.linkText("Home"));
         waitMillis(1000);
+        home.click();
         System.out.println(driver.getTitle());
         assertEquals("CashyLand - Home", driver.getTitle());
     }
@@ -34,8 +28,8 @@ class NavigationTest {
         WebElement accedi = null;
         accedi = driver.findElement(By.id("loginLi"));
         WebElement loginLink = accedi.findElement(By.id("loginBtn"));
-        loginLink.click();
         waitMillis(1000);
+        loginLink.click();
         System.out.println(driver.getTitle());
         assertEquals("CashyLand - Login", driver.getTitle());
     }
@@ -44,8 +38,8 @@ class NavigationTest {
     void registrati() {
         WebElement registrati = null;
         registrati = driver.findElement(By.name("register"));
-        registrati.click();
         waitMillis(1000);
+        registrati.click();
         System.out.println(driver.getTitle());
         assertEquals("CashyLand - Registrazione", driver.getTitle());
     }
@@ -54,8 +48,8 @@ class NavigationTest {
     void forgot() {
         WebElement forgot = null;
         forgot = driver.findElement(By.linkText("Hai dimenticato la password?"));
-        forgot.click();
         waitMillis(1000);
+        forgot.click();
         System.out.println(driver.getTitle());
         assertEquals("CashyLand - Password Smarrita?", driver.getTitle());
     }
@@ -64,8 +58,8 @@ class NavigationTest {
     void giochi() {
         WebElement giochi = null;
         giochi = driver.findElement(By.linkText("Giochi"));
-        giochi.click();
         waitMillis(1000);
+        giochi.click();
         System.out.println(driver.getTitle());
         assertEquals("CashyLand - Giochi", driver.getTitle());
     }
@@ -74,8 +68,8 @@ class NavigationTest {
     void sale() {
         WebElement sale = null;
         sale = driver.findElement(By.linkText("Sale"));
-        sale.click();
         waitMillis(1000);
+        sale.click();
         System.out.println(driver.getTitle());
         assertEquals("CashyLand - Sale", driver.getTitle());
     }
@@ -88,7 +82,6 @@ class NavigationTest {
         WebDriverWait wait = new WebDriverWait(driver, 60);
         wait.until(ExpectedConditions.elementToBeClickable(By.className("container")));
         driver.get(URL);
-        waitMillis(1000);
         System.out.println(driver.getTitle());
         assertEquals("CashyLand - Home", driver.getTitle());
 
@@ -101,15 +94,6 @@ class NavigationTest {
         giochi();
         home();
         sale();
-        home();
-
-        WebElement giochi = null;
-        giochi = driver.findElement(By.linkText("Clicca qui per scoprirne di pìù"));
-        giochi.click();
-        waitMillis(1000);
-        System.out.println(driver.getTitle());
-        assertEquals("CashyLand - Giochi", driver.getTitle());
-
         home();
         System.out.println("OK");
 
